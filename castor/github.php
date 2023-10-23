56cad67d54d374b90fa2d89041623f981c31b8e2
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2023-10-23 14:04:20 +0200

    feat: attributes options

--- castor/github.php
+++ castor/github.php
@@ -3,28 +3,30 @@
 namespace MonsieurBiz\SyliusSetup\Castor\Github;
 
 use Castor\Attribute\AsTask;
+use Castor\Attribute\AsOption;
 
 use function Castor\io;
 use function Castor\run;
 
 #[AsTask(name: 'init', namespace: 'github:env', description: 'Init environment (and variables if needed)')]
-function initGithubEnv(): void
-{
+function initGithubEnv(
+    #[AsOption(description: 'Environment', suggestedValues: ['prod', 'staging'])] ?string $env = null,
+    #[AsOption(description: 'Deployment branch', suggestedValues: ['master'])] ?string $branch = null,
+): void {
     $token = trim(run('gh auth token', quiet: true)->getOutput());
     $repo = trim(run('gh repo view --json nameWithOwner --jq .nameWithOwner | cat', quiet: true)->getOutput());
-    $environment = io()->ask('Which environment?', 'staging');
+    $environment = $env ?? io()->ask('Which environment?', 'staging');
     $command = <<<CMD
     curl --silent -L \
     -X PUT \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $token" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/$repo/environments/$environment \
     -d '{"deployment_branch_policy":{"protected_branches":false,"custom_branch_policies":true}}'
     CMD;
     run($command, quiet: true);
 
-    $branch = io()->ask('Deployment branch?', 'master');
+    $branch = $branch ?? io()->ask('Deployment branch?', 'master');
     run(sprintf(
         'gh api --silent --method POST -H "Accept: application/vnd.github+json" "/repos/%1$s/environments/%2$s/deployment-branch-policies" -f name=%3$s',
         $repo,
